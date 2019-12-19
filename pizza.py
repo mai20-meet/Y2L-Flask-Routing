5fa0e36e27ca0b16b2c2f12cfc2004a438551b9f
myname21-meet <myname21@meet.mit.edu>
2019-12-19 11:49:49 -0500

    iwanttodie

--- pizza.py
+++ pizza.py
@@ -1,7 +1,25 @@
-switch = 2
-while switch < 101:
-	light = 100 %switch
-	switch+=1
-	print(light)
+checked_number = 57
+for i in range(2, checked_number):
+	if checked_number%i !=0:
+		checked_number == False:
+		print(str(checked_number) + " is a prime number ")
+	elif checked_number%i !=0:
+		checked_number == False:
+		print(str(checked_number) + " is not a prime number ")
 
-	
+
+
+
+
+
+
+
+
+
+
+
+
+	# 	print(str(checked_number) + " is a prime number ")
+	# else:
+	# 	print(str(checked_number) + " is not a prime number ")
+
